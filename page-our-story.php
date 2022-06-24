9eae6cdddb2f57a53f2e254446dc049d6c538d63
JayGangkun <jaygangkun@hotmail.com>
2022-06-23 21:20:26 -0400

    our story page

---
+++ page-our-story.php
@@ -0,0 +1,62 @@
+<?php
+/**
+* Template Name: Our Story Page
+*
+* @package WordPress
+* @subpackage 
+* @since 
+*/
+
+?>
+<?php get_header() ?>
+<section class="section-story-hero">
+	<div class="container-md">
+		<h1 class="story-hero-title fs-50"><span class="text-highlight">At Dudley & Co</span>, we're <span class="f-italic">mad about dogs.</span> In fact, we love our dog so much we named the company after him.</h1>
+	</div>
+</section>
+<section class="section-story-content">
+	<img class="w-100" src="<?php echo get_template_directory_uri()?>/assets/images/ourstory-img1.png">
+	<div class="container-md">
+		<p class="section-story-text">Raised in a warm climate, Dudley & Co co-founder Amanda didn’t realise dogs needed cold-weather clothing until she moved to London and met Rizzo, a short-haired Vizsla. He was adorable and Amanda – unable to have a pup due to travel commitments - asked friends if she could dog-sit. Like all Vizslas, he was a sensitive soul who hated the cold, and slept in a tight ball during London’s long winters. One chilly day, Amanda walked into a pet store in Notting Hill and bought Rizzo his first coat. Did he love it? Did he ever! He even slept in it, his legs stretched out, languidly and happily. Amanda saw the importance of dog clothing and its role as more than just window-dressing for pooches, and started to look closely at what clothing and accessories were available for dogs. </p>
+		<div class="section-story-content-imgs">
+			<div class="section-story-content-img-col">
+				<img class="w-100" src="<?php echo get_template_directory_uri()?>/assets/images/ourstory-img2.png">
+			</div>
+			<div class="section-story-content-img-col">
+				<img class="w-100" src="<?php echo get_template_directory_uri()?>/assets/images/ourstory-img3.png">
+			</div>
+		</div>
+		<p class="section-story-text">Fast forward to 2016. Amanda, now living in Switzerland, had adopted Dudley, a gorgeous Vizsla puppy. To prepare him for the nippy winters, Amanda bought an indoor jumper, an outdoor coat and little rubber booties to wear on the ski slopes. Before long, Dudley had amassed quite a wardrobe. But too much of the apparel was flawed. Clothing wasn’t durable enough. Zips broke, fabric stretched or tore, and buttons and clasps came off. Stronger pieces were often made with cheap cloth, while ‘luxury’ fabrics were often poorly fitted and finished. Amanda spotted a gap in the market: a glaring need for practical, stylish and durable clothing for dogs, using high-quality fabrics, custom-made by the best British and European factories.<br><br>
+		Armed with a great idea and with entrepreneurial vim running through her veins, Amanda just needed a partner to bring her vision to life. A chance phone call from long-time friend Lori Heiss-Tiplady changed everything. Lori had years of hard-boiled experience working in branding and product development at some of the world’s largest firms, and was looking for a fresh challenge. Lori was the missing piece of the puzzle, and so it was that Dudley & Co was born.<br><br>
+		Our mission is to create a high end range of durable, reliable and stylish clothing and accessories for dogs. All our products are proudly made in Britain and designed to live up to the promise of. </p>
+	</div>
+</section>
+
+<section class="section-story-tasks">
+	<div class="container-lg">
+		<h1 class="fs-50 section-story-tasks-title"><span class="text-highlight">Luxury that lasts</span></h1>
+		<div class="section-story-tasks-list">
+			<div class="section-story-task-col">
+				<div class="section-story-task-img" style="background-image:url(<?php echo get_template_directory_uri()?>/assets/images/story-task-img1.png)"></div>
+				<h6 class="section-story-task-title">✓ Putting Dogs First</h6>
+				<p class="section-story-task-desc">Dudley & Co's products are designed with dogs in mind. Our fabrics and fasteners are built tough but with luxury in mind - your dog's comfort is always paramount. </p>
+			</div>
+			<div class="section-story-task-col">
+				<div class="section-story-task-img" style="background-image:url(<?php echo get_template_directory_uri()?>/assets/images/story-task-img2.png)"></div>
+				<h6 class="section-story-task-title">✓ A Family Affair</h6>
+				<p class="section-story-task-desc">Our products come from the heart. Dudley & Co is named after our in-house pooch, while our signature coat is in loving memory of Rizzo, a London Vizsla from Notting Hill who inspired our dream. We aim to name future clothing after our customers' dogs as they join the family.</p>
+			</div>
+			<div class="section-story-task-col">
+				<div class="section-story-task-img" style="background-image:url(<?php echo get_template_directory_uri()?>/assets/images/story-task-img3.png)"></div>
+				<h6 class="section-story-task-title">✓ Doggie Do-Gooders</h6>
+				<p class="section-story-task-desc">At Dudley & Co we care deeply about our four-legged friends. A share of our profits will go to independent dog charities on both sides of the Atlantic.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<script>
+	jQuery(document).ready(function() {
+		
+	})
+</script>
+<?php get_footer() ?>
